9641be62a59a9ac7f586518039e03287e93bb61b
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-04-04 01:57:32 +0530

    Creating REST API using Spring Boot

--- src/main/java/being/rish/journalApp/JournalApplication.java
+++ src/main/java/being/rish/journalApp/JournalApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // Rest API: Representational state transfer Application programming interfaces
-
+// HTTP verb -> GET(See), PUT(Modify), POST(Create), DELETE(Delete)
+// GET 172.17.18.19:8080/ntflx/plans(Http verb + IP + Port + end-point => Rest API)
 @SpringBootApplication
 public class JournalApplication {
 
