3487cec19d1bda21915df129e81c9330bb86e014
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-04-04 00:03:45 +0530

    First commit

---
+++ src/main/java/being/rish/journalApp/JournalApplication.java
@@ -0,0 +1,15 @@
+package being.rish.journalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Rest API: Representational state transfer Application programming interfaces
+
+@SpringBootApplication
+public class JournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalApplication.class, args);
+	}
+
+}
