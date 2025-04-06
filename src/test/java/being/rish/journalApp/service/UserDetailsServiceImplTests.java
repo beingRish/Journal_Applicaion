91ba05281b6e12069f0056e211156c32c2e37210
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-04-07 01:21:19 +0530

    Mockito | @Mock @InjectMocks

---
+++ src/test/java/being/rish/journalApp/service/UserDetailsServiceImplTests.java
@@ -0,0 +1,37 @@
+package being.rish.journalApp.service;
+
+import being.rish.journalApp.entity.User;
+import being.rish.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
+
+public class UserDetailsServiceImplTests {
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void loadUserByUsernameTest() {
+        when(userRepository.findByUserName(ArgumentMatchers.anyString())).thenReturn(User.builder().userName("rishabh").password("jkl").roles(new ArrayList<>()).build());
+        UserDetails user = userDetailsService.loadUserByUsername("rishabh");
+        Assertions.assertNotNull(user);
+    }
+}
