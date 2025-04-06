497c86639340091c4383fa53db2aab0b228ba9c5
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-04-07 00:52:06 +0530

    JUnit Testing in Spring Boot | @CsvSource @ArgumentsSource @SpringBootTest

---
+++ src/test/java/being/rish/journalApp/service/UserArgumentsProvider.java
@@ -0,0 +1,21 @@
+package being.rish.journalApp.service;
+
+import being.rish.journalApp.entity.User;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
+
+public class UserArgumentsProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+        return Stream.of(
+                Arguments.of(User.builder().userName("vipul").password("vipul").build()),
+                Arguments.of(User.builder().userName("raj").password("raj").build())
+
+        );
+    }
+}
---
+++ src/test/java/being/rish/journalApp/service/UserServiceTests.java
@@ -0,0 +1,42 @@
+package being.rish.journalApp.service;
+
+import being.rish.journalApp.entity.User;
+import being.rish.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserServiceTests {
+
+    @Autowired
+    public UserRepository userRepository;
+
+    @Autowired
+    public UserService userService;
+
+    @ParameterizedTest
+    @ArgumentsSource(UserArgumentsProvider.class)
+    public void testSaveNewUser(User user) {
+        assertTrue(userService.saveNewUser(user));
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,2",
+            "2,10,12",
+            "3,3,6"
+    })
+    public void test(int a, int b, int expected){
+        assertEquals(expected, a+b);
+    }
+
+}
