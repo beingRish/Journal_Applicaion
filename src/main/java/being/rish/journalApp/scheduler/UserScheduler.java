4dd643412a778c493880d6efa28631ddc1c0b936
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-04-11 00:45:50 +0530

    Scheduling Tasks with Cron Jobs in Spring Boot

--- src/main/java/being/rish/journalApp/scheduler/UserScheduler.java
+++ src/main/java/being/rish/journalApp/scheduler/UserScheduler.java
@@ -1,4 +1,47 @@
 package being.rish.journalApp.scheduler;
 
+import being.rish.journalApp.cache.AppCache;
+import being.rish.journalApp.entity.JournalEntry;
+import being.rish.journalApp.entity.User;
+import being.rish.journalApp.repository.UserRepositoryImpl;
+import being.rish.journalApp.service.EmailService;
+import being.rish.journalApp.service.SentimentAnlysisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserScheduler {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserRepositoryImpl userRepository;
+
+    @Autowired
+    private SentimentAnlysisService sentimentAnlysisService;
+
+    @Autowired
+    private AppCache appCache;
+
+    @Scheduled(cron = "0 0 9 * * SUN")
+    public void fetchUsersAndSendSaMail() {
+        List<User> users = userRepository.getUserForSA();
+        for(User user: users) {
+            List<JournalEntry> journalEntries = user.getJournalEntries();
+            List<String> filteredEntries = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x->x.getContent()).collect(Collectors.toList());
+            String entry = String.join(" ", filteredEntries);
+            String sentiment = sentimentAnlysisService.getSentiment(entry);
+            emailService.sendEmail(user.getEmail(), "Sentiment for last 7 days", sentiment);
+        }
+    }
+
+    @Scheduled(cron = "0 0/10 * ? * *")
+    public void clearAppCache() {
+        appCache.init();
+    }
 }
--- src/main/java/being/rish/journalApp/service/SentimentAnlysisService.java
+++ src/main/java/being/rish/journalApp/service/SentimentAnlysisService.java
@@ -1,4 +1,10 @@
 package being.rish.journalApp.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SentimentAnlysisService {
+    public String getSentiment(String text) {
+        return "";
+    }
 }
